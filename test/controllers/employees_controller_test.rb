ab5f8322bb4d114a9d60097b1d10509ae3caea96
alfie <alfie.2012@gmail.com>
2015-08-16 17:15:31 +0530

    Tests to authorize employee creation

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -1,11 +1,25 @@
 require 'test_helper'
 
 describe EmployeesController do
+  let(:admin) { User.create(email: 'admin@test.com', password: 'password', is_admin: true)}
+  let(:user_email) {'test@user.com'}
+
   describe "#create" do
     it 'creates a user' do
-      user_email = 'test@user.com'
+      sign_in admin
       post :create, user: {email: user_email}
-      assert_equal User.last.email, user_email
+
+      user = User.last
+      assert_equal user.email, user_email
+      refute user.is_admin?
+
+      assert_redirected_to root_path
+    end
+
+    it 'prevents access if not admin' do
+      assert_no_difference 'User.count' do
+        post :create, user: {email: user_email}
+      end
 
       assert_redirected_to root_path
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/spec'
 require "minitest/rails"
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
